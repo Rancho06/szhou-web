c7c75cd7b32c20a62f78b5d7ea3a2605295aa966
Shu Zhou <shuzhou@usc.edu>
2015-12-22 19:20:22 -0500

    improve indentation

--- gulpfile.js
+++ gulpfile.js
@@ -19,53 +19,53 @@ let coffeeSources = './components/coffee/*.coffee',
 
 /* converts all .coffee into .js scripts */
 gulp.task('coffee', function() {
-	gulp.src(coffeeSources)
+    gulp.src(coffeeSources)
         .pipe(sourcemaps.init())
         .pipe(coffee({ bare : true }).on('error', gutil.log))
         .pipe(sourcemaps.write())
-		.pipe(gulp.dest('components/scripts'));
+        .pipe(gulp.dest('components/scripts'));
 });
 
 /* converts all .sass into .css and then combines them to a single .css file */
 gulp.task('sass', function() {
-	return sass(sassSources, {
-			sourcemap: true
-		})
-		.on('error', sass.logError)
-		.pipe(sourcemaps.write())
-		.pipe(concat('style.css'))
-		.pipe(gulp.dest('builds/css'));
+    return sass(sassSources, {
+            sourcemap: true
+        })
+        .on('error', sass.logError)
+        .pipe(sourcemaps.write())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('builds/css'));
 });
 
 /* combines all .js scripts to a single uglified js file */
 gulp.task('js', function() {
-	gulp.src(jsSources)
-		.pipe(uglify())
-		.pipe(concat('script.js'))
-		.pipe(gulp.dest('builds/js'));
+    gulp.src(jsSources)
+        .pipe(uglify())
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/js'));
 });
 
 /* start the express app */
 gulp.task('express', function() {
-	require('./server');
+    require('./server');
 });
 
 /* monitor the changes in source files */
 gulp.task('watch', function() {
-	gulp.watch(coffeeSources, ['coffee']);
-	gulp.watch(sassSources, ['sass']);
-	gulp.watch(jsSources, ['js']);
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(sassSources, ['sass']);
+    gulp.watch(jsSources, ['js']);
 });
 
 /* start the webserver with livereload */
 gulp.task('webserver', function() {
-	gulp.src('./builds')
-		.pipe(webserver({
+    gulp.src('./builds')
+        .pipe(webserver({
 			host: 'localhost',
 			port: 3000,
 			livereload: true,
 			open: true,
-		}));
+        }));
 });
 
 /* the default task when running 'gulp' from command line */
